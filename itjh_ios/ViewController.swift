8e2b9aad284cbfde0ab0439bcce8233d6be54cbc
itjhDev <git@itjh.com.cn>
2015-02-03 21:36:42 +0800

    初始化
    初始化

---
+++ itjh_ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  itjh_ios
+//
+//  Created by LijunSong on 15/2/3.
+//  Copyright (c) 2015年 LijunSong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
